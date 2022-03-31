72ee4f5be879d824342b35435e3e2753d849dd3e
Kris Huber <internet@krishuber.xyz>
2022-03-31 09:06:28 +0200

    Added Login Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Box, Button, Heading, Stack } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) navigate(-1);
+  }, [isAuthenticated]);
+
+  return (
+    <Box>
+      <Stack maxW={'50%'}>
+        <Heading fontSize={24}>Wie benutzt du diese Platform?</Heading>
+        <Button
+          onClick={() => loginWithRedirect({ connection: 'google-oauth2' })}
+        >
+          Als Talent
+        </Button>
+        <Button onClick={() => loginWithRedirect({ connection: 'talentpool' })}>
+          Als Firmenmitglied
+        </Button>
+        <Button
+          onClick={() => loginWithRedirect({ connection: 'google-oauth2' })}
+        >
+          Als Administrator
+        </Button>
+      </Stack>
+    </Box>
+  );
 };
