4c6615a75e9384153b0a5d8a5ca4fb5136d773f6
Kris Huber <internet@krishuber.xyz>
2022-04-05 14:14:36 +0200

    Old Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,159 @@
-import React, { useEffect } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Box, Button, Heading, Stack } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
+import React, { SyntheticEvent, useState } from 'react';
+import {
+  Button,
+  Input,
+  Flex,
+  Heading,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  Box,
+  Link,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+  Center,
+} from '@chakra-ui/react';
+import '../styles/login.css';
+import { FaUserAlt, FaLock } from 'react-icons/fa';
+import { BiShow, BiHide } from 'react-icons/bi';
+import { GoogleLogin } from 'react-google-login';
 
+// TODO: forgot password
 export const Login = () => {
-  const { loginWithRedirect, user } = useAuth0();
-  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  useEffect(() => {
-    if (user) navigate(-1);
-  }, [user]);
+  /*
+   * 1. Enter Email
+   * 2. Get User Role
+   * 3. Show Google/Password
+   */
+
+  const responseGoogle = (res: any) => {
+    console.log(res);
+  };
+
+  const submitEmail = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    setAuthMethod(await getAuthMethod(email));
+  };
+
+  const submitLogin = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log('Logged in: ' + JSON.stringify({ email, password }));
+  };
 
   return (
-    <Box>
-      <Stack maxW={'50%'}>
-        <Heading fontSize={24}>Wie benutzt du diese Platform?</Heading>
-        <Button
-          onClick={() => loginWithRedirect({ connection: 'google-oauth2' })}
-        >
-          Als Talent
-        </Button>
-        <Button onClick={() => loginWithRedirect({ connection: 'talentpool' })}>
-          Als Firmenmitglied
-        </Button>
-        <Button
-          onClick={() => loginWithRedirect({ connection: 'google-oauth2' })}
-        >
-          Als Administrator
-        </Button>
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor="gray.200"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+        onSubmit={submitEmail}
+      >
+        <Heading>Login</Heading>
+        <Box minW={{ base: '90%', md: '468px' }}>
+          <form>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            >
+              {/* Email */}
+              <FormControl isRequired>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<FaUserAlt style={{ fill: 'lightgray' }} />}
+                  />
+                  <Input
+                    type="email"
+                    placeholder="Email Adresse"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </InputGroup>
+              </FormControl>
+
+              {authMethod != null ? (
+                authMethod === AuthMethod.INTERNAL ? (
+                  // % % Password % %
+                  <FormControl onSubmit={submitLogin}>
+                    <InputGroup>
+                      <InputLeftElement
+                        pointerEvents="none"
+                        color="gray.300"
+                        children={<FaLock style={{ fill: 'lightgray' }} />}
+                      />
+                      <Input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Passwort"
+                        onChange={(e) => {
+                          setPassword(e.target.value);
+                        }}
+                      />
+                      <InputRightElement width="3.1rem">
+                        <Button
+                          h="1.75rem"
+                          size="sm"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? <BiHide /> : <BiShow />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    <FormHelperText textAlign="right">
+                      <Link>Passwort vergessen?</Link>
+                    </FormHelperText>
+                  </FormControl>
+                ) : (
+                  // % % Google % %
+                  <Center>
+                    <Flex>
+                      <GoogleLogin
+                        clientId="88947083280-p9vmftafikev3e1m2scct856bdtvj5mm.apps.googleusercontent.com"
+                        render={(renderProps) => (
+                          <Button
+                            onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}
+                          >
+                            Weiter mit Google
+                          </Button>
+                        )}
+                        buttonText="Login"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                      />
+                    </Flex>
+                  </Center>
+                )
+              ) : (
+                <></>
+              )}
+
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                width="full"
+              >
+                Weiter
+              </Button>
+            </Stack>
+          </form>
+        </Box>
       </Stack>
-    </Box>
+    </Flex>
   );
 };
