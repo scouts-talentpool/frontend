68104f3701160da5425ae960414e37ce931a9c10
Kris Huber <internet@krishuber.xyz>
2022-04-26 09:29:55 +0200

    Talents List

--- src/pages/TalentProfile.tsx
+++ src/pages/TalentProfile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const TalentProfile = () => {
-  return <div>TalentProfile</div>;
+  return <div>TalentProfile (of ...)</div>;
 };
--- src/pages/Talents.tsx
+++ src/pages/Talents.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Skeleton, Stack } from '@chakra-ui/react';
+import { getTalents } from '../lib/talents';
+import { useQueryClient, useQuery } from 'react-query';
+import { ProfileCard } from '@/components/base/ProfileCard';
 
 export const Talents = () => {
-  return <div>Talents</div>;
+  const { getAccessTokenSilently } = useAuth0();
+  const queryClient = useQueryClient();
+
+  const talents = useQuery('talents', () => {
+    return getAccessTokenSilently().then(
+      async (accessToken: string) => await getTalents(accessToken),
+    );
+  });
+
+  if (talents.isLoading) {
+    return <Skeleton isLoaded={false}></Skeleton>;
+  }
+
+  if (talents.isError) {
+    return <span>Error:</span>;
+  }
+
+  return (
+    <Stack>
+      {talents.data.map((talent: any) => (
+        <ProfileCard
+          id={talent.id}
+          imgUrl="https://www.horn-talent.de/wp-content/uploads/2018/08/cat-banken-versicherungen-final-300x200.jpg"
+          title={talent.name}
+          location={talent.location}
+          about={'about lorem ipsum dolor'}
+          key={talent.id}
+        />
+      ))}
+    </Stack>
+  );
 };
