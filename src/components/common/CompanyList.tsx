6a8c45e44b510e0bd287cf2fbbc37a36a7c50589
Kris Huber <internet@krishuber.xyz>
2022-04-27 15:24:20 +0200

    CompanyList Refactor

---
+++ src/components/common/CompanyList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Skeleton, Stack } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+import { Company } from '@/components/common/Company';
+import aspida from '@aspida/axios';
+import api from '@/api/$api';
+import { Navigate } from 'react-router-dom';
+
+export const CompanyList = () => {
+  const client = api(aspida());
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  const companies = useQuery('companies', async () => {
+    return getAccessTokenSilently().then(
+      async (accessToken: string) =>
+        await client.companies.$get({
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }),
+    );
+  });
+
+  if (companies.isLoading) {
+    return <Skeleton isLoaded={false}></Skeleton>;
+  }
+
+  if (companies.isError) {
+    return <Navigate to={`/error?message=${companies.error}`} />;
+  }
+
+  return (
+    <Stack>
+      {companies.data?.map((company) => (
+        <Company key={company.id} company={company} />
+      ))}
+    </Stack>
+  );
+};
