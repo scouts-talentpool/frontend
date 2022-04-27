0616a0a16f4829483025c7a9773060840a8de2e9
Kris Huber <internet@krishuber.xyz>
2022-04-27 16:00:55 +0200

    Redoing doing doing when im in a build i

--- src/components/admin/CompaniesPanel.tsx
+++ src/components/admin/CompaniesPanel.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { CompanyList } from '../common/CompanyList';
+import { Box, Center, Stack, Image, Flex } from '@chakra-ui/react';
+import { BaseButton } from '../base/BaseButton';
 
 export const CompaniesPanel = () => {
-  return <div>CompaniesPanel</div>;
+  return (
+    <>
+      {/* actions */}
+      <Box
+        bg={'gray.100'} 
+        m={4}
+        p={0}
+        borderWidth="1px"
+        borderRadius="lg"
+        direction={{ base: 'column', md: 'row' }}
+        boxShadow="md"
+        padding={4}
+        rounded="0px"
+      >
+        <BaseButton>Create Company</BaseButton>
+        <BaseButton>Edit Company</BaseButton>
+      </Box>
+
+      <CompanyList />
+    </>
+  );
 };
