70aae4695a08bce7b8faf9a6228b00538b0fd919
Lian Studer <ln.studer@protonmail.ch>
2022-04-28 09:40:42 +0200

    feat: paginate all lists

--- src/components/common/CompanyList.tsx
+++ src/components/common/CompanyList.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Skeleton, Stack } from '@chakra-ui/react';
-import { useQuery } from 'react-query';
+import { Button, Flex, HStack, Spinner, Stack } from '@chakra-ui/react';
+import { useQuery, useQueryClient } from 'react-query';
 import { Company } from '@/components/common/Company';
 import aspida from '@aspida/axios';
 import api from '@/api/$api';
@@ -10,30 +10,66 @@ import { Navigate } from 'react-router-dom';
 export const CompanyList = () => {
   const client = api(aspida());
 
+  const [cursor, setCursor] = useState<number>(1);
+  const [take, _] = useState<number>(25);
+
   const { getAccessTokenSilently } = useAuth0();
+  const queryClient = useQueryClient();
 
-  const companies = useQuery('companies', async () => {
-    return getAccessTokenSilently().then(
-      async (accessToken: string) =>
-        await client.companies.$get({
-          headers: { Authorization: `Bearer ${accessToken}` },
-        }),
-    );
-  });
+  const companies = useQuery(
+    ['companies', cursor],
+    async () => {
+      return getAccessTokenSilently().then(
+        async (accessToken: string) =>
+          await client.companies.$get({
+            headers: { Authorization: `Bearer ${accessToken}` },
+            query: {
+              take,
+              cursor,
+            },
+          }),
+      );
+    },
+    { keepPreviousData: true },
+  );
 
-  if (companies.isLoading) {
-    return <Skeleton isLoaded={false}></Skeleton>;
-  }
+  if (companies.isLoading) return <Spinner />;
+
+  if (companies.isPreviousData) return <Spinner />;
 
   if (companies.isError) {
     return <Navigate to={`/error?message=${companies.error}`} />;
   }
 
   return (
-    <Stack>
-      {companies.data?.map((company) => (
-        <Company key={company.id} company={company} />
-      ))}
-    </Stack>
+    <Flex direction="column">
+      <Stack>
+        {companies.data?.map((company) => (
+          <Company key={company.id} company={company} />
+        ))}
+      </Stack>
+      <HStack mt="4">
+        <Button
+          onClick={() => {
+            if (cursor > 0) {
+              setCursor((old) => old - take);
+            }
+          }}
+          disabled={cursor === 1}
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={() => {
+            if (!companies.isPreviousData) {
+              setCursor((old) => old + take);
+            }
+          }}
+          disabled={companies.isPreviousData}
+        >
+          Next
+        </Button>
+      </HStack>
+    </Flex>
   );
 };
