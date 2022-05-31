e981ecd44237cd6d15564ab102f691718b1f53e9
Kris Huber <kris@krishuber.xyz>
2022-05-31 13:13:46 +0200

    Tabbed Home Component and Timeline dummy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,53 @@
-import { Image } from '@chakra-ui/image';
 import React from 'react';
-import bus from '@/assets/bus.jpg';
-import { Center, Stack, Heading} from '@chakra-ui/react';
+import { Container,
+  Heading,
+  Tab,
+  Tabs,
+  TabPanel,
+  TabPanels,
+  TabList
+} from '@chakra-ui/react';
+import { TimelineEntry } from '@/components/base/TimelineEntry';
 
+/*
+  maybe split into multiple components => (User is Logged in homepage, Not logged in homepage)
+  or make `/home` a protected route with login page redirect
+ */
 export const Home = () => {
   return (
-    <Center>
-    	<Heading size="sm"></Heading>
-    </Center>
+    <Container maxW='60%'>
+      <Tabs>
+        <TabList>
+          <Tab>Timeline</Tab>
+          <Tab>Explore</Tab>
+          <Tab>Following</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Heading>Timeline</Heading>
+            {/*
+              Timeline with events of all followd companies / talents
+              
+              3x Dummy Entries Below:
+            */}
+            <TimelineEntry />
+            <TimelineEntry />
+            <TimelineEntry />
+          </TabPanel>
+          <TabPanel>
+            <Heading>Explore</Heading>
+            {/*
+              Logic to render either companies if talent or talents if company 
+              */}
+          </TabPanel>
+          <TabPanel>
+            <Heading>Following</Heading>
+            {/*
+              On this tab display the followed talents / companies
+            */}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Container>
   );
 };
